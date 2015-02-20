331afff684247288b45f00ccd13bda2585b1496f
Rommel Rico <rommeltj@gmail.com>
2015-02-19 18:23:43 -0800

    Adding a Share Extension

---
+++ D61ShareExt/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  D61ShareExt
+//
+//  Created by Rommel Rico on 2/19/15.
+//  Copyright (c) 2015 Rommel Rico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
