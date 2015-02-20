b9ba4746e0076414880edefea058b3431ce8ba23
Rommel Rico <rommeltj@gmail.com>
2015-02-19 17:54:16 -0800

    Initial Commit

---
+++ D61ContainingAppTests/D61ContainingAppTests.m
@@ -0,0 +1,40 @@
+//
+//  D61ContainingAppTests.m
+//  D61ContainingAppTests
+//
+//  Created by Rommel Rico on 2/19/15.
+//  Copyright (c) 2015 Rommel Rico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface D61ContainingAppTests : XCTestCase
+
+@end
+
+@implementation D61ContainingAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
---
+++ README.md
@@ -0,0 +1,20 @@
+# Contains an App Extension
+
+Version: 0.0.1 - 19 Feb 2015
+
+## Description
+
+iOS Programming II at UCSD Extension.
+
+### Documentation
+Branches:  
+master = Production branch  
+dev = Development branch  
+
+## Contact
+
+<me@rommelrico.com>
+
+## Notes
+
+If you want to contribute, email me at <me@rommelrico.com>.
